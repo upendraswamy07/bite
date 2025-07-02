ce968d42605afad8ef201217ea33295b4a602e21
upendraswamy07 <upendraswamy05@gmail.com>
2025-07-02 13:58:39 +0530

    fix: use env for backend URL

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import Navbar from './components/Nabar/Navbar'
+import React from 'react';
+import Navbar from './components/Nabar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar'; 
 
-import { Route, Routes } from 'react-router-dom'
-import Add from './pages/Add/Add'
-import List from './pages/List/List'
-import Orders from './pages/Orders/Orders'
+import { Route, Routes } from 'react-router-dom';
+import Add from './pages/Add/Add';
+import List from './pages/List/List';
+import Orders from './pages/Orders/Orders';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-
-  const URl = "http://localhost:4000"
-
+  // Use environment variable instead of hardcoding
+  const URl = import.meta.env.VITE_API_URL;
 
   return (
     <div>
       <ToastContainer />
-      <Navbar/>
+      <Navbar />
       <hr />
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path='/add' element={<Add URl={URl}/>}/>
-          <Route path='/list' element={<List URl={URl}/>}/>
-          <Route path='/orders' element={<Orders URl={URl}/>}/>
+          <Route path="/add" element={<Add URl={URl} />} />
+          <Route path="/list" element={<List URl={URl} />} />
+          <Route path="/orders" element={<Orders URl={URl} />} />
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
